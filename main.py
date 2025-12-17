494c9b2362c91975b55f3ca430a020eb0d83293b
Prometeo <xairi.valdivia@gmail.com>
2025-12-17 10:45:35 -0500

    feat: recursive methods

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ class BinarySearchTree:
                     return True
                 temp = temp.right
 
-    def containse(self, value) -> bool:
+    def contains(self, value: int) -> bool:
         temp = self.root
         while temp is not None:
             if value < temp.value:
@@ -39,3 +39,55 @@ class BinarySearchTree:
             else:
                 return True
         return False
+
+    # Recursive methods
+    def __r_contains(self, current_node: Node | None, value: int):
+        if current_node is None:
+            return False
+        if value == current_node.value:
+            return True
+        if value < current_node.value:
+            return self.__r_contains(current_node.left, value)
+        if value > current_node.value:
+            return self.__r_contains(current_node.right, value)
+
+    def r_contains(self, value: int):
+        return self.__r_contains(self.root, value)
+
+    def __r_insert(self, current_node: Node | None, value: int):
+        if current_node is None:
+            return Node(value)
+        if value < current_node.value:
+            current_node.left = self.__r_insert(current_node.left, value)
+        if value > current_node.value:
+            current_node.right = self.__r_insert(current_node.right, value)
+        return current_node
+
+    def r_insert(self, value):
+        self.__r_insert(self.root, value)
+
+    def min_value(self, current_node: Node):
+        while current_node.left is not None:
+            current_node = current_node.left
+
+        return current_node.value
+
+    def __delete_node(self, current_node: Node, value: int):
+        if current_node is None:
+            return None
+        if value < current_node.value:
+            current_node.left = self.__delete_node(current_node.left, value)
+        elif value > current_node.value:
+            current_node.right = self.__delete_node(current_node.right, value)
+        else:
+            if current_node.left is None and current_node.right is None:
+                return None
+            elif current_node.left is None:
+                current_node = current_node.right
+            elif current_node.right is None:
+                current_node = current_node.left
+            else:
+                sub_tree_min = self.min_value(current_node.right)
+                current_node.value = sub_tree_min
+                current_node.right = self.__delete_node(current_node.right, sub_tree_min)
+        return current_node
